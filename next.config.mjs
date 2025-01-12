fa68d790052a306def940bc117e29cc83aae052a
devczero <devczero@gmail.com>
2025-01-12 16:40:21 +0100

    depl fix 3

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true, // Ignores ESLint errors/warnings during build
-    },
-  };
-  
-  export default nextConfig;
-  
