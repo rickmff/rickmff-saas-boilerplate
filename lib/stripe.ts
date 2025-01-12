79eabc18eaef208a4ffcfaeebf69729122b2a010
devczero <devczero@gmail.com>
2025-01-12 20:16:20 +0100

    depl fix a

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe"
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-    apiVersion: "2024-09-30.acacia",
+    apiVersion: "2024-12-18.acacia", // Updated to the correct version
     typescript: true
 })
 
